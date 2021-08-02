2b0186c7a464614d8f62d9212961c1ea804b1f53
Oscarvch03 <oscar.velasco@urosario.edu.co>
2021-08-02 08:22:37 -0500

    Código Servidor

---
+++ Taller2/Servidor/TCPservidor.py
@@ -0,0 +1,65 @@
+from socket import *
+
+def saldo():
+    file = open("saldo.txt", 'r')
+    sld = file.readlines()[-1]
+    file.close()
+    return sld
+
+def debitar(X):
+    file = open("saldo.txt", 'a')
+    sld = float(saldo())
+    ans = ""
+    if(X <= sld):
+        file.write("\n" + str(sld - X))
+        ans += "OK"
+    else:
+        ans += "Saldo Insuficiente"
+    file.close()
+    return ans
+
+def acreditar(Y):
+    file = open("saldo.txt", 'a')
+    sld = float(saldo())
+    ans = str(sld + Y) 
+    file.write("\n" + ans)
+    file.close()
+    ans = "Nuevo Saldo => " + ans
+    return ans
+
+
+servidorPuerto = 12000
+servidorSocket = socket(AF_INET, SOCK_STREAM)
+servidorSocket.bind(('', servidorPuerto))
+servidorSocket.listen(1)
+print("El servidor está listo para recibir mensajes. \n")
+
+while(1):
+    conexionSocket, clienteDireccion = servidorSocket.accept()
+    # print("Conexión establecida con ", clienteDireccion)
+    mensaje = str(conexionSocket.recv(1024), "utf-8")
+    print("Mensaje recibido de: ", clienteDireccion)
+    msg = mensaje.split()
+    print(msg)
+
+    mensajeRespuesta = ""
+
+    if(msg[0] == "saldo"):
+        mensajeRespuesta += saldo()
+    elif(msg[0] == "debitar"):
+        X = float(msg[1])
+        mensajeRespuesta += debitar(X)
+    elif(msg[0] == "acreditar"):
+        Y = float(msg[1])
+        mensajeRespuesta += acreditar(Y)
+    elif(msg[0] == "q"):
+        break
+    else:
+        mensajeRespuesta += "No se pudo realizar la Consulta. "
+
+    # print(mensaje)
+    # mensajeRespuesta = mensaje.upper()
+
+    print("Mensaje respuesta:", mensajeRespuesta, "\n")
+    conexionSocket.send(bytes(mensajeRespuesta, "utf-8"))
+    conexionSocket.close()
